ce2ae645d169b861849ae27cdf9ddeb7e26de3d8
Eugene <evg_kod@mail.ru>
2017-10-07 12:43:36 +0700

    Asteroids minor improve

--- Assets/Scripts/AsteroidsSpawn.cs
+++ Assets/Scripts/AsteroidsSpawn.cs
@@ -14,6 +14,7 @@ public class AsteroidsSpawn : MonoBehaviour {
 	public float asteroidClipDistance = 1;
 	private float asteroidDistanceSqr;
 	private float asteroidClipDistanceSqr;
+	public float asteroidRadius = 1;
 
 	private int updateEvery = 5;
 	private int counter = 0;
@@ -37,9 +38,17 @@ public class AsteroidsSpawn : MonoBehaviour {
 		for (int i = 0; i < asteroidsMax; i++)
 		{
 			asteroids[i] = Instantiate(prefabs[Random.Range(0, prefabs.Length)]) as GameObject;
-			asteroids[i].transform.position = Random.insideUnitCircle * asteroidDistance + (Vector2)tx.position;
+			Vector2 pos = Random.insideUnitCircle * asteroidDistance + (Vector2)tx.position;
+			while (Physics2D.OverlapCircle(pos, asteroidRadius)) {
+				pos = Random.insideUnitCircle * asteroidDistance + (Vector2)tx.position;
+			}
+		
+			//asteroids[i].transform.position = Random.insideUnitCircle * asteroidDistance + (Vector2)tx.position;
+			//asteroids[i].transform.position = Random.insideUnitCircle * asteroidDistance + (Vector2)tx.position;
+			asteroids[i].transform.position = pos;
 			asteroids[i].transform.parent = root.transform;
 			//asteroids[i].GetComponent<Rigidbody2D>().angularVelocity = Random.insideUnitCircle * 2f;
+			asteroids[i].GetComponent<Rigidbody2D>().angularVelocity = Random.Range(-100f, 100f);
 		}
 	}
 	
