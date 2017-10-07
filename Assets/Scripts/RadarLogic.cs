12c0f68501333a286666650fe47d4017052c6579
Eugene <evg_kod@mail.ru>
2017-10-07 21:51:03 +0700

    Radar logic

---
+++ Assets/Scripts/RadarLogic.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarLogic : MonoBehaviour {
+	public GameObject dot;
+	public Dictionary<string, GameObject> dots;
+	public float vecCoef = 0.1f;
+	Dictionary<string, GameObject> capsules = SpaceGameState.Instance.CapsuleManager.SpawnedCapsules;
+	// Use this for initialization
+	void Start () {
+		
+		//dots = new GameObject[capsules.Count];
+		foreach (var item in capsules) {
+			GameObject d = Instantiate (dot);
+			dots.Add(item.Key, d);
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (var item in dots) {
+			item.Value.transform.position = GetDotPosition (item.Key);
+		}
+	}
+
+	Vector3 GetDotPosition(string key){
+		GameObject c = null;
+		Vector3 capsulePos = new Vector3(0,0,0);
+		if (capsules.TryGetValue (key, out c)) {
+			capsulePos = c.transform.position;
+			Vector3 playerPos = GameObject.FindGameObjectWithTag ("Player").transform.position;
+			Vector3 relativePos = playerPos - capsulePos;
+			relativePos *= vecCoef;
+			return relativePos;
+		} else
+			return capsulePos;
+
+		//Vector3.ClampMagnitude(relativePos, 
+	}
+}
