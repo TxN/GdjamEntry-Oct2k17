fd70c512564cdcf6d301e9babfeb4e7fbda2c44a
Eugene <evg_kod@mail.ru>
2017-10-07 23:55:50 +0700

    Radar main logic that works+Active capsule

--- Assets/Scripts/RadarLogic.cs
+++ Assets/Scripts/RadarLogic.cs
@@ -5,34 +5,51 @@ using UnityEngine;
 public class RadarLogic : MonoBehaviour {
 	public GameObject dot;
 	public Dictionary<string, GameObject> dots;
-	public float vecCoef = 0.1f;
-	Dictionary<string, GameObject> capsules = SpaceGameState.Instance.SpawnedCapsules;
+	public float vecCoef = 100f;
+	//Dictionary<string, GameObject> capsules = SpaceGameState.Instance.SpawnedCapsules;
+	Dictionary<string, GameObject> capsules;
+
+	private bool first = true;
 	// Use this for initialization
 	void Start () {
-		
-		//dots = new GameObject[capsules.Count];
-		foreach (var item in capsules) {
-			GameObject d = Instantiate (dot);
-			dots.Add(item.Key, d);
-		}
+
 	}
 
 	
 	// Update is called once per frame
 	void Update () {
-		foreach (var item in dots) {
-			item.Value.transform.position = GetDotPosition (item.Key);
+		if (first) {
+			first = false;
+			capsules = SpaceGameState.Instance.SpawnedCapsules;
+			dots = new Dictionary<string, GameObject>();
+			foreach (var item in capsules) {
+				GameObject d = Instantiate (dot);
+				d.SetActive (true);
+				d.transform.SetParent (this.transform);
+				dots.Add(item.Key, d);
+			}
 		}
+		if (dots.Count != 0) {
+			foreach (var item in dots) {
+				item.Value.transform.localPosition = GetDotPosition (item.Key);
+			}
+		}
+
 	}
 
 	Vector3 GetDotPosition(string key){
 		GameObject c = null;
 		Vector3 capsulePos = new Vector3(0,0,0);
+		//Vector3 interfaceShift = new Vector3 (590, 200, 0);
 		if (capsules.TryGetValue (key, out c)) {
 			capsulePos = c.transform.position;
 			Vector3 playerPos = GameObject.FindGameObjectWithTag ("Player").transform.position;
-			Vector3 relativePos = playerPos - capsulePos;
+			//Vector3 relativePos = playerPos - capsulePos;
+			Vector3 relativePos =  capsulePos - playerPos;
+
+
 			relativePos *= vecCoef;
+			//relativePos = new Vector3 (relativePos.x + interfaceShift.x, relativePos.y + interfaceShift.y, 0);
 			return relativePos;
 		} else
 			return capsulePos;
