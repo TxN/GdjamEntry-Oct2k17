e1f7e042d78b97779dc176dc540e81db1b801a1a
Eugene <evg_kod@mail.ru>
2017-10-08 13:21:00 +0700

    tex

--- Assets/Scripts/GameLogic/Consequences.cs
+++ Assets/Scripts/GameLogic/Consequences.cs
@@ -52,4 +52,20 @@ public class Consequences : MonoBehaviour {
         output += "\n\n2017\nArtGames Jam Novosibirsk";
         return output;
     }
+
+	void CreateAftermath(){
+		Dictionary<string, string> finalTexts = new Dictionary<string, string> () {
+			{ "0 0 0 0", "After a long sophisticated journey to the new planet settlers had finally reached the ground of their new home. They gathered around the spaceship and sat for a treaty that could bear their existence as happy and stable society. Each of them stood in front of the crowd and made a speech about the role of an expedition in the history of humanity. So they decided that they should restore all traditions of their predecessors: remember all rituals, read all books. Under the sky of a new planet should rise modern architecture and ancient sculptural masterpiece." },
+			{"1 0 0 0", "After the landing on the planet Eart-2 great leader of colonization, comrade Roderick got into special position of power to lead and protect Humanity within its new home. Since then, he and his successors  were named “Big Comrade”, and Roderick was greatest of them. He established fair distribution of goods  and rights, where more rights goes with more responsibility!  “No clericalism for brave men in arms, he said, no primates of individuality above social needs!”"},
+			{"1 1 0 0", "After the long terrifying journey to the planet named Paradise, the captain of the Ark, Roderick the First, lead the faithful society  to the heathen on earth by fire and sword of his own.  Righteous men of God fought and sacrificed heretics, who were worshipping horrible satanic “science”, since that struggle everyone started to live in peace and love."},
+			{"1 1 1 0", ""}
+		};
+
+		int[] final_state = { 0, 0, 0, 0 };
+		if (_state.IsOnBoard("Soldier")) {final_state[0] = 1;}
+		if (_state.IsOnBoard("Preacher") & _state.IsOnBoard("Poet")) {final_state[1] = 1;}
+		if (_state.IsOnBoard("Buisnessman")) {final_state[2] = 1;}
+		if (_state.IsOnBoard("Engineer")) {final_state[3] = 1;}
+		if ((_state.IsOnBoard("TeacherWoman") | _state.IsOnBoard("Teacher")) & _state.IsOnBoard("Farmer")) {final_state[3] = 0;}
+	}
 }
